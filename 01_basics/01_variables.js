a5aa367f362401a48d854634ffcf7e6a10ac806d
devshanuuu <shanuuu0207@gmail.com>
2025-06-25 19:22:15 +0000

    Variables file added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "shanuuu0207@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "445"
+accountCity = "Delhi"
+
+console.log(accountId);
+
+// prefer not to use var
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
