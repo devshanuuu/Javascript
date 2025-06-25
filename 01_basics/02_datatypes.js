c0aec2b9a532901ebca04a2fbcbe645bd8b0308f
devshanuuu <shanuuu0207@gmail.com>
2025-06-25 19:51:41 +0000

    Datatypes file added

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+/* alert(3+3) --> will not work in Node.js, only in browser */
+
+let name = "Shanu";
+let age = 22;
+let isLoggenIn = false
+
+// number => 2 to pwer 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value, no value
+// undefined => variable is not defined
+// symbol => unique
+
+// objects
+
+console.log(typeof "shanu");
+console.log(typeof 4);
