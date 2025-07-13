6ec6a9625a0b2951f79cf8b97f8037d6ec6a6312
devshanuuu <shanuuu0207@gmail.com>
2025-07-13 14:29:06 +0000

    datatype summary file

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,41 @@
+// Primitve type
+
+/*7 types : String, Number, Boolean, null, undefined,
+ symbol, BigInt */   
+const score = 100
+const scoreValue = 100.3
+
+const isLogged = false
+const outsideTemp = null
+let useremail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId); // false(every call to symbol creates a unique and distinct symbol)
+
+const bigNumber = 52342444234234423n
+
+
+// Reference type (Non primitive)
+
+/* Array, Objects, Functions */
+const heros = ["ironman", "flash", "hulk"];
+
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world")
+}
+
+// Datatype know
+console.log(typeof scoreValue); // => number
+console.log(typeof outsideTemp); // => object(typeof of null is object)
+console.log(typeof myFunction); // => function
+console.log(typeof heros); // => object
+
+
+
+
